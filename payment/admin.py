69085328bf2fefc95ab14d488f0d626fd26035d4
mbgoodguy <nerooren4001@gmail.com>
2024-02-13 23:27:06 +0300

    Add yookassa webhook and OrderAdmin
    Exclude all __pycache__

--- payment/admin.py
+++ payment/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 
 from payment.models import ShippingAddress, Order, OrderItem
 
 # Register your models here.
-admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
+
+
+@admin.register(Order)
+class OrderAdmin(ModelAdmin):
+    list_display = ('id', 'user', 'shipping_address', 'amount', 'created', 'updated', 'paid')
+    list_filter = ['created', 'updated', 'paid']
+    search_fields = ['user__username']
+
+
--- payment/webhooks.py
+++ payment/webhooks.py
@@ -4,6 +4,7 @@ import stripe
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from yookassa import Webhook
 
 from payment.models import Order
 
@@ -19,7 +20,8 @@ def stripe_webhook(request):
     '''
 
     payload = request.body  # Get the body of the HTTP request that contains the JSON-formatted data sent by Stripe.
-    sig_header = request.META['HTTP_STRIPE_SIGNATURE']  # Retrieve the HTTP_STRIPE_SIGNATURE header that contains the request signature created by Stripe.
+    sig_header = request.META[
+        'HTTP_STRIPE_SIGNATURE']  # Retrieve the HTTP_STRIPE_SIGNATURE header that contains the request signature created by Stripe.
     event = None
 
     try:
@@ -53,4 +55,77 @@ def stripe_webhook(request):
     return HttpResponse(status=200)
 
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
+
+def yookassa_webhook(request):
+    webhook = Webhook(request.body, request.headers['Content-Type'])
+    event = webhook.parse()
+
+    # handle payment event
+    if event.type == 'payment.succeeded':
+        return HttpResponse(status=200)
+
+    return HttpResponse(status=200)
+
+    ip = get_client_ip(request)  # get request ip
+    if not SecurityHelper().is_ip_trusted(ip):
+        return HttpResponse(status=400)
+
+    # extract JSON object from request body
+    event_json = json.loads(request.body)
+    try:
+        # creating a notification class object based on an event
+        notification_object = WebhookNotificationFactory().create(event_json)
+        response_object = notification_object.object
+        if notification_object.event == WebhookNotificationEventType.PAYMENT_SUCCEEDED:
+            some_data = {
+                'paymentId': response_object.id,
+                'paymentStatus': response_object.status,
+            }
+            # specific logic
+            # ...
+
+        elif notification_object.event == WebhookNotificationEventType.PAYMENT_CANCELED:
+            some_data = {
+                'paymentId': response_object.id,
+                'paymentStatus': response_object.status,
+            }
+        else:
+            # errors handle
+            return HttpResponse(status=400)  # send info about error to YooKassa
+
+        Configuration.configure('XXXXXX', 'test_XXXXXXXX')
+        # get actual info about payment
+        payment_info = Payment.find_one(some_data['paymentId'])
+
+        if payment_info:
+            try:
+                order_id = payment_info['id']
+            except Order.DoesNotExist:
+                return HttpResponse(status=404)
+
+            payment_status = payment_info.status
+
+            # specific logic
+            if payment_status == 'succeeded':
+                order = Order.objects.get(id=order_id)
+                order.paid = True
+                order.save()
+            else:
+                raise ValueError('Payment status is unsuccessful. An error has occurred')
+        else:
+            # error handle
+            return HttpResponse(status=400)  # send info about error to YooKassa
+
+    except Exception:
+        # error handle
+        return HttpResponse(status=400)  # send info about error to YooKassa
+
+    return HttpResponse(status=200)
