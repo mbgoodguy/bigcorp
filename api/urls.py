6bff760a3f864091b9e9ba05ebf09e84879dafc0
mbgoodguy <nerooren4001@gmail.com>
2024-02-28 21:56:07 +0300

    Add swagger/redoc documentation

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,24 @@
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from . import views
 
 app_name = 'api'
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="example@admin.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
 
 urlpatterns = [
     path('products/', views.ProductListApiView.as_view()),
@@ -11,4 +27,7 @@ urlpatterns = [
     path('reviews/create/', views.ReviewCreateView.as_view()),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
